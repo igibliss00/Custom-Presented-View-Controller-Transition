73c7a7eabe333dafaa7446e2e1b76fe8c7254282
igibliss00@gmail.com <igibliss00@gmail.com>
2020-12-21 17:40:13 -0500

    Add UISnapBehavior

--- YourApp/ViewControllers/FavoriteVC.swift
+++ YourApp/ViewControllers/FavoriteVC.swift
@@ -7,11 +7,81 @@
 
 import UIKit
 
-class FavoriteVC: UIViewController {
-
+class FavoriteVC: UIViewController, UIPageViewControllerDataSource {
+    let galleries: [String] = ["1", "2", "3", "4", "5", "6", "7"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        let sp = SinglePage(gallery: galleries[0])
+        
+        let pvc = PageViewController(transitionStyle: .scroll, navigationOrientation: .horizontal, options: nil)
+        pvc.setViewControllers([sp], direction: .forward, animated: false, completion: nil)
+        pvc.dataSource = self
+        
+        addChild(pvc)
+        self.view.addSubview(pvc.view)
+        pvc.view.frame = self.view.bounds
+        pvc.didMove(toParent: self)
+        
+        let proxy = UIPageControl.appearance()
+        proxy.pageIndicatorTintColor = UIColor.gray.withAlphaComponent(0.6)
+        proxy.currentPageIndicatorTintColor = .black
+        proxy.backgroundColor = .clear
     }
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+        guard let gallery = (viewController as! SinglePage).gallery, var index = galleries.firstIndex(of: gallery) else { return nil }
+        index -= 1
+        if index < 0 {
+            return nil
+        }
+        return SinglePage(gallery: galleries[index])
+    }
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+        guard let gallery = (viewController as! SinglePage).gallery, var index = galleries.firstIndex(of: gallery) else { return nil }
+        index += 1
+        if index >= galleries.count {
+            return nil
+        }
+        return SinglePage(gallery: galleries[index])
+    }
+    
+    func presentationCount(for pageViewController: UIPageViewController) -> Int {
+        return self.galleries.count
+    }
+    
+    func presentationIndex(for pageViewController: UIPageViewController) -> Int {
+        let page = pageViewController.viewControllers![0] as! SinglePage
+        let gallery = page.gallery!
+        return self.galleries.firstIndex(of: gallery)!
+    }
+}
 
+class PageViewController: UIPageViewController {
+    
+}
+
+class SinglePage: UIViewController {
+    var gallery: String!
+    
+    init(gallery: String) {
+        self.gallery = gallery
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let size = UIScreen.main.bounds.size
+        let image = UIImage(named: "\(gallery ?? "1").jpg")!
+        let imageView = UIImageView(frame: CGRect(origin: .zero, size: CGSize(width: size.width, height: size.height)))
+        imageView.image = image
+        imageView.contentMode = .scaleAspectFill
+        self.view.addSubview(imageView)
+    }
 }
--- YourApp/ViewControllers/PlaylistVC.swift
+++ YourApp/ViewControllers/PlaylistVC.swift
@@ -8,10 +8,48 @@
 import UIKit
 
 class PlaylistVC: UIViewController {
-
+    let v = UIImageView(image: UIImage(named: "5.jpg"))
+    var animator: UIDynamicAnimator!
+    var snapping: UISnapBehavior!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+//        self.v.image = UIImage(named: "5.jpg")
+        self.view.addSubview(self.v)
+        self.v.isUserInteractionEnabled = true
+        self.v.layer.cornerRadius = 20
+        self.v.clipsToBounds = true
+        self.v.translatesAutoresizingMaskIntoConstraints = false
+        
+        self.animator = UIDynamicAnimator(referenceView: self.view)
+        self.snapping = UISnapBehavior(item: self.v, snapTo: self.view.center)
+        self.animator.addBehavior(self.snapping)
+        
+        let pan = UIPanGestureRecognizer(target: self, action: #selector(panned))
+        self.v.addGestureRecognizer(pan)
+        
+        NSLayoutConstraint.activate([
+            v.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
+            v.centerYAnchor.constraint(equalTo: self.view.centerYAnchor),
+            v.widthAnchor.constraint(equalToConstant: 200),
+            v.heightAnchor.constraint(equalToConstant: 300)
+        ])
     }
-
+    
+    @objc func panned(_ sender: UIPanGestureRecognizer) {
+        switch sender.state {
+            case .began:
+                self.animator.removeBehavior(self.snapping)
+            case .changed:
+                let translation = sender.translation(in: self.v.superview)
+                self.v.center = CGPoint(x: self.v.center.x + translation.x, y: self.v.center.y + translation.y)
+                sender.setTranslation(.zero, in: self.v.superview)
+            case .ended, .cancelled, .failed:
+                self.animator.addBehavior(self.snapping)
+            case .possible:
+                break
+            default:
+                break
+        }
+    }
 }
